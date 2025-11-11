1ba132ad6a80214f6410a965e123908151dc0951
nick <zkac222@live.rhul.ac.uk>
2025-11-11 19:37:42 +0000

    Initial commit

--- utils/clerkTokenCache.ts
+++ utils/clerkTokenCache.ts
@@ -1,13 +1,11 @@
-import * as SecureStore from 'expo-secure-store';
+import * as SecureStore from 'expo-secure-store'
 
 export type TokenCache = {
-  getToken: (key: string) => Promise<string | null>;
-  saveToken: (key: string, value: string) => Promise<void>;
-};
+  getToken: (key: string) => Promise<string | null>
+  saveToken: (key: string, value: string) => Promise<void>
+}
 
 export const tokenCache: TokenCache = {
   getToken: (key) => SecureStore.getItemAsync(key),
   saveToken: (key, value) => SecureStore.setItemAsync(key, value),
-};
-
-
+}
